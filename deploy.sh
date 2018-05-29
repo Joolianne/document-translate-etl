1cfddbf15ea1cf1d3077dadcc30289874e8845a2
Julianne <usethefork@gmail.com>
2018-05-29 14:29:12 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,4 @@
-
+func_name=getText
+GCLOUD_PROJECT=`gcloud config get-value project`
+OCR_IN_BUCKET="${GCLOUD_PROJECT}__ocr-in"
+time gcloud functions deploy $func_name --trigger-resource $OCR_IN_BUCKET --trigger-event google.storage.object.finalize --timeout 540
